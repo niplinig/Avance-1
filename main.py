eae5f1a427e09d8ba0f78273b6a2778636518a64
Replit user <>
2023-06-12 00:14:11 +0000

    algoritmo y correcciones

--- main.py
+++ main.py
@@ -21,6 +21,62 @@ def insertion_sort(array)
 end
 """
 
+# Algoritmo Merge Sort
+"""
+def merge_sort(array)
+  if array.length <= 1
+    return array
+  end
+
+  array_size = array.length
+  middle = (array.length / 2).round
+
+  left_side = array[0...middle]
+  right_side = array[middle...array_size]
+
+  sorted_left = merge_sort(left_side)
+  sorted_right = merge_sort(right_side)
+
+  merge(array, sorted_left, sorted_right)
+
+  return array
+end
+
+def merge(array, sorted_left, sorted_right)
+  left_size = sorted_left.length
+  right_size = sorted_right.length
+
+  array_pointer = 0
+  left_pointer = 0
+  right_pointer = 0
+
+  while left_pointer < left_size && right_pointer < right_size
+    if sorted_left[left_pointer] < sorted_right[right_pointer]
+      array[array_pointer] = sorted_left[left_pointer]
+      left_pointer+=1
+    else
+      array[array_pointer] = sorted_right[right_pointer]
+      right_pointer+=1
+    end
+    array_pointer+=1
+  end
+
+  while left_pointer < left_size
+      array[array_pointer] = sorted_left[left_pointer]
+      left_pointer+=1
+      array_pointer+=1
+  end
+
+  while right_pointer < right_size
+     array[array_pointer] = sorted_right[right_pointer]
+     right_pointer+=1
+     array_pointer+=1
+  end
+
+  return array
+end
+"""
+
 #SELECT * FROM Tabla
 #SELECT campo1, campo2 from Tabla1 where campo==1
 #SELECT campo1 as cedula from Datos where provincia<>"7"
@@ -63,6 +119,7 @@ tokens = (
 		'LESSTH',
 		'GREATH',
 		'COMMA',
+		'AMPERSAND',
   'ID',
 ) + tuple(reserved.values())
  
@@ -83,6 +140,7 @@ t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_LESSTH = r'\<'
 t_GREATH = r'\>'
+t_AMPERSAND = r'\&'
  
  #Para contabilizar nro de líneas
 def t_newline(t):
@@ -115,21 +173,57 @@ lexer = lex.lex()
 
 #Testeando
 data = """
-def insertion_sort(array)
-    for i in 1...(array.length)  # Step 1
-        j = i # Step 2
-        while j > 0 # Step 3
-            if array[j-1] > array[j] # Step 4
-                temp = array[j]
-                array[j] = array[j-1]
-                array[j-1] = temp
-            else
-                break
-            end
-            j = j - 1 # Step 5
-        end
-    end
+def merge_sort(array)
+  if array.length <= 1
     return array
+  end
+
+  array_size = array.length
+  middle = (array.length / 2).round
+
+  left_side = array[0...middle]
+  right_side = array[middle...array_size]
+
+  sorted_left = merge_sort(left_side)
+  sorted_right = merge_sort(right_side)
+
+  merge(array, sorted_left, sorted_right)
+
+  return array
+end
+
+def merge(array, sorted_left, sorted_right)
+  left_size = sorted_left.length
+  right_size = sorted_right.length
+
+  array_pointer = 0
+  left_pointer = 0
+  right_pointer = 0
+
+  while left_pointer < left_size && right_pointer < right_size
+    if sorted_left[left_pointer] < sorted_right[right_pointer]
+      array[array_pointer] = sorted_left[left_pointer]
+      left_pointer+=1
+    else
+      array[array_pointer] = sorted_right[right_pointer]
+      right_pointer+=1
+    end
+    array_pointer+=1
+  end
+
+  while left_pointer < left_size
+      array[array_pointer] = sorted_left[left_pointer]
+      left_pointer+=1
+      array_pointer+=1
+  end
+
+  while right_pointer < right_size
+     array[array_pointer] = sorted_right[right_pointer]
+     right_pointer+=1
+     array_pointer+=1
+  end
+
+  return array
 end
 """
  
