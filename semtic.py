578d412a71e648d9a7d1c1cdec5ce4baebf83ba8
Nicolás Plaza <niplinig@espol.edu.ec>
2023-06-25 21:49:52 -0500

    Adding sematic

--- semtic.py
+++ semtic.py
@@ -6,9 +6,38 @@
 # Analizador Semántico
 # ----------------------------------
 
+import ply.yacc as yacc
+from main import lexer, tokens
+
+
+def p_numeric_int(p):
+    "numeric : INT"
+
+
+def p_numeric_float(p):
+    "numeric : FLOAT"
+
+
+def p_addition(p):
+    "addition : numeric PLUS numeric"
+
+
+def p_substrac(p):
+    "substrac : numeric MINUS numeric"
+
 
 def main():
-    print("Hello, World")
+    parser = yacc.yacc(debug=1)
+
+    while True:
+        try:
+            command = input(" >  ")
+        except EOFError:
+            break
+        if not command:
+            continue
+        result = parser.parse(command)
+        print(result)
 
 
 if __name__ == "__main__":
