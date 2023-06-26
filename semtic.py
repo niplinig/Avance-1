1c26cd0114e89dd7988f0d3eff9ca7b8ac589ddb
Nicolás Plaza <niplinig@espol.edu.ec>
2023-06-25 21:30:47 -0500

    Adding semantic py

---
+++ semtic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# ----------------------------------
+# other.py
+#
+# Analizador Semántico
+# ----------------------------------
+
+
+def main():
+    print("Hello, World")
+
+
+if __name__ == "__main__":
+    main()
